8bdc33414f4085f9f049907400bff36715b18ef3
Sonali Patil <118295011+Sonali2314@users.noreply.github.com>
2024-05-04 13:37:08 +0530

    Hello.py

--- Hello.py
+++ Hello.py
@@ -1,3 +1,3 @@
-//First programe of repositry so its basics of python 
+#First programe of repositry so its basics of python 
 s=input("Enter Your Name : ")
 print("Hello ",s,"\nHave a nice day :)")
