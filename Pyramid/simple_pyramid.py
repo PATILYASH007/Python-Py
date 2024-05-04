2e633008944e61e5cb2c2fe4ada676f5761f007f
Sonali Patil <118295011+Sonali2314@users.noreply.github.com>
2024-05-04 22:15:40 +0530

    simple pyramid
    *
    **
    ***
    ****
    Simple pyramid

---
+++ Pyramid/simple_pyramid.py
@@ -0,0 +1,10 @@
+print("\t\tSimple Pyramid\n")
+n=int(input("Enter the no of rows : "))
+a=input("Enter the string for pyramid :")
+s=1
+while s in range(1,n):
+{
+    d=a*s
+    print(d)
+    s=s+1
+}
